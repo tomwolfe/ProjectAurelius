48672a8c8b6548187537dc3c1a592ce4d994d733
Tom <tomwolfe@gmail.com>
2026-06-22 18:49:36 -0500

    Convert certification-lab to git submodule pointing to private repo

--- certification-lab/src/certifier/__init__.py
+++
@@ -1,5 +0,0 @@
-from certifier.optimizer import KernelOptimizer
-from certifier.validator import UncertaintyAuditor
-from certifier.signer import KernelSigner
-
-__all__ = ["KernelOptimizer", "UncertaintyAuditor", "KernelSigner"]
